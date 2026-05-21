4eec0f69e526534028d3ae5cadd23a68a746f39e
Dr. Tavinho <gusrmp13@gmail.com>
2026-05-21 18:20:13 -0300

    Initial commit

---
+++ TicTacToe/Form1.Designer.vb
@@ -0,0 +1,373 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.lblTurn = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblTittle = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtName1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtName2 = New System.Windows.Forms.TextBox()
+        Me.lblVitoria = New System.Windows.Forms.Label()
+        Me.btnReplay = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.lblPoint1 = New System.Windows.Forms.Label()
+        Me.lblPoint2 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.chbBot = New System.Windows.Forms.CheckBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn1.Location = New System.Drawing.Point(53, 116)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(100, 100)
+        Me.btn1.TabIndex = 0
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn2.Location = New System.Drawing.Point(151, 116)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(100, 100)
+        Me.btn2.TabIndex = 1
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn3.Location = New System.Drawing.Point(249, 116)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(100, 100)
+        Me.btn3.TabIndex = 2
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn4.Location = New System.Drawing.Point(53, 214)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(100, 100)
+        Me.btn4.TabIndex = 4
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn5.Location = New System.Drawing.Point(151, 214)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(100, 100)
+        Me.btn5.TabIndex = 5
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn9.Location = New System.Drawing.Point(249, 312)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(100, 100)
+        Me.btn9.TabIndex = 6
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn6.Location = New System.Drawing.Point(249, 214)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(100, 100)
+        Me.btn6.TabIndex = 9
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn8.Location = New System.Drawing.Point(151, 312)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(100, 100)
+        Me.btn8.TabIndex = 8
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn7
+        '
+        Me.btn7.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btn7.Location = New System.Drawing.Point(53, 312)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(100, 100)
+        Me.btn7.TabIndex = 7
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'lblTurn
+        '
+        Me.lblTurn.AutoSize = True
+        Me.lblTurn.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblTurn.Location = New System.Drawing.Point(53, 88)
+        Me.lblTurn.Name = "lblTurn"
+        Me.lblTurn.Size = New System.Drawing.Size(129, 25)
+        Me.lblTurn.TabIndex = 11
+        Me.lblTurn.Text = "Vez de: NULL"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.LightSeaGreen
+        Me.PictureBox1.Location = New System.Drawing.Point(-4, -6)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(811, 98)
+        Me.PictureBox1.TabIndex = 12
+        Me.PictureBox1.TabStop = False
+        '
+        'lblTittle
+        '
+        Me.lblTittle.AutoSize = True
+        Me.lblTittle.BackColor = System.Drawing.Color.LightSeaGreen
+        Me.lblTittle.Font = New System.Drawing.Font("Franklin Gothic Medium", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblTittle.Location = New System.Drawing.Point(223, 18)
+        Me.lblTittle.Name = "lblTittle"
+        Me.lblTittle.Size = New System.Drawing.Size(371, 61)
+        Me.lblTittle.TabIndex = 13
+        Me.lblTittle.Text = "JOGO DA VELHA"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Semibold", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(493, 254)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(162, 30)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "JOGADOR 1 (X):"
+        '
+        'txtName1
+        '
+        Me.txtName1.Location = New System.Drawing.Point(493, 287)
+        Me.txtName1.Name = "txtName1"
+        Me.txtName1.Size = New System.Drawing.Size(223, 23)
+        Me.txtName1.TabIndex = 16
+        Me.txtName1.Text = "Jogador 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI Semibold", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(493, 324)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(169, 30)
+        Me.Label2.TabIndex = 18
+        Me.Label2.Text = "JOGADOR 2 (O):"
+        '
+        'txtName2
+        '
+        Me.txtName2.Location = New System.Drawing.Point(493, 357)
+        Me.txtName2.Name = "txtName2"
+        Me.txtName2.Size = New System.Drawing.Size(223, 23)
+        Me.txtName2.TabIndex = 19
+        Me.txtName2.Text = "Jogador 2"
+        '
+        'lblVitoria
+        '
+        Me.lblVitoria.AutoEllipsis = True
+        Me.lblVitoria.BackColor = System.Drawing.Color.White
+        Me.lblVitoria.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblVitoria.Dock = System.Windows.Forms.DockStyle.Top
+        Me.lblVitoria.Font = New System.Drawing.Font("Segoe UI", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblVitoria.Location = New System.Drawing.Point(0, 0)
+        Me.lblVitoria.Name = "lblVitoria"
+        Me.lblVitoria.Size = New System.Drawing.Size(804, 88)
+        Me.lblVitoria.TabIndex = 20
+        Me.lblVitoria.Text = "VITÓRIA DE JOGADOR1"
+        Me.lblVitoria.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lblVitoria.Visible = False
+        '
+        'btnReplay
+        '
+        Me.btnReplay.BackColor = System.Drawing.Color.White
+        Me.btnReplay.Font = New System.Drawing.Font("Segoe UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnReplay.Location = New System.Drawing.Point(239, 206)
+        Me.btnReplay.Name = "btnReplay"
+        Me.btnReplay.Size = New System.Drawing.Size(328, 75)
+        Me.btnReplay.TabIndex = 21
+        Me.btnReplay.Text = "JOGAR NOVAMENTE?"
+        Me.btnReplay.UseVisualStyleBackColor = False
+        Me.btnReplay.Visible = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.MediumTurquoise
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI Semibold", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(532, 127)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 50)
+        Me.Label3.TabIndex = 22
+        Me.Label3.Text = "X"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.MediumTurquoise
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI Semibold", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(612, 127)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(50, 50)
+        Me.Label4.TabIndex = 24
+        Me.Label4.Text = "O"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.MediumTurquoise
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI Semibold", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.Location = New System.Drawing.Point(574, 127)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(37, 50)
+        Me.Label5.TabIndex = 26
+        Me.Label5.Text = "-"
+        '
+        'lblPoint1
+        '
+        Me.lblPoint1.AutoSize = True
+        Me.lblPoint1.BackColor = System.Drawing.Color.White
+        Me.lblPoint1.Font = New System.Drawing.Font("Segoe UI Semibold", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblPoint1.Location = New System.Drawing.Point(532, 177)
+        Me.lblPoint1.Name = "lblPoint1"
+        Me.lblPoint1.Size = New System.Drawing.Size(45, 50)
+        Me.lblPoint1.TabIndex = 27
+        Me.lblPoint1.Text = "X"
+        '
+        'lblPoint2
+        '
+        Me.lblPoint2.AutoSize = True
+        Me.lblPoint2.BackColor = System.Drawing.Color.White
+        Me.lblPoint2.Font = New System.Drawing.Font("Segoe UI Semibold", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblPoint2.Location = New System.Drawing.Point(616, 177)
+        Me.lblPoint2.Name = "lblPoint2"
+        Me.lblPoint2.Size = New System.Drawing.Size(45, 50)
+        Me.lblPoint2.TabIndex = 28
+        Me.lblPoint2.Text = "X"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(355, 350)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(60, 60)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 30
+        Me.PictureBox2.TabStop = False
+        '
+        'chbBot
+        '
+        Me.chbBot.AutoSize = True
+        Me.chbBot.Location = New System.Drawing.Point(493, 386)
+        Me.chbBot.Name = "chbBot"
+        Me.chbBot.Size = New System.Drawing.Size(94, 19)
+        Me.chbBot.TabIndex = 31
+        Me.chbBot.Text = "Computador"
+        Me.chbBot.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.MediumTurquoise
+        Me.ClientSize = New System.Drawing.Size(804, 461)
+        Me.Controls.Add(Me.chbBot)
+        Me.Controls.Add(Me.btnReplay)
+        Me.Controls.Add(Me.lblVitoria)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.lblPoint2)
+        Me.Controls.Add(Me.lblPoint1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtName2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtName1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblTittle)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.lblTurn)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(820, 500)
+        Me.MinimumSize = New System.Drawing.Size(820, 500)
+        Me.Name = "Form1"
+        Me.Text = "Jogo Da Velha"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents lblTurn As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lblTittle As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtName1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtName2 As TextBox
+    Friend WithEvents lblVitoria As Label
+    Friend WithEvents btnReplay As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lblPoint1 As Label
+    Friend WithEvents lblPoint2 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents chbBot As CheckBox
+End Class
---
+++ TicTacToe/Form1.vb
@@ -0,0 +1,302 @@
+﻿Public Class Form1
+
+    Dim turno As Integer = 0
+    Dim p1point As Integer = 0
+    Dim p2point As Integer = 0
+    Dim emjogo As Boolean = True
+    Dim bot As Boolean = False
+    Dim cont As Integer = 0
+    Dim play1 As String = "Jogador 1"
+    Dim play2 As String = "Jogador 2"
+    Dim mtx(9) As Button
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        mtx(0) = btn1
+        mtx(1) = btn2
+        mtx(2) = btn3
+        mtx(3) = btn4
+        mtx(4) = btn5
+        mtx(5) = btn6
+        mtx(6) = btn7
+        mtx(7) = btn8
+        mtx(8) = btn9
+
+        For i As Integer = 0 To 8
+            mtx(i).Text = ""
+        Next
+
+        ' Não está funcionando corretamente
+        If bot Then
+            ' chbBot.Checked = True
+        Else
+            ' chbBot.Checked = False
+        End If
+
+        btnReplay.Visible = False
+        lblVitoria.Visible = False
+
+        txtName1.Text = play1
+        txtName2.Text = play2
+
+        lblPoint1.Text = CStr(p1point)
+        lblPoint2.Text = CStr(p2point)
+
+        emjogo = True
+        cont = 0
+
+        turno = 1
+        trocaturno()
+
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        If turno = 0 And mtx(0).Text = "" And emjogo Then
+            btn1.Text = "X"
+            cont += 1
+            checkvit()
+            trocaturno()
+        ElseIf turno = 1 And mtx(0).Text = "" And emjogo Then
+            btn1.Text = "O"
+            cont += 1
+            checkvit()
+            trocaturno()
+        End If
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        If turno = 0 And mtx(1).Text = "" And emjogo Then
+            btn2.Text = "X"
+            cont += 1
+            checkvit()
+            trocaturno()
+        ElseIf turno = 1 And mtx(1).Text = "" And emjogo Then
+            btn2.Text = "O"
+            cont += 1
+            checkvit()
+            trocaturno()
+        End If
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        If turno = 0 And mtx(2).Text = "" And emjogo Then
+            btn3.Text = "X"
+            cont += 1
+            checkvit()
+            trocaturno()
+        ElseIf turno = 1 And mtx(2).Text = "" And emjogo Then
+            btn3.Text = "O"
+            cont += 1
+            checkvit()
+            trocaturno()
+        End If
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        If turno = 0 And mtx(3).Text = "" And emjogo Then
+            btn4.Text = "X"
+            cont += 1
+            checkvit()
+            trocaturno()
+        ElseIf turno = 1 And mtx(3).Text = "" And emjogo Then
+            btn4.Text = "O"
+            cont += 1
+            checkvit()
+            trocaturno()
+        End If
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        If turno = 0 And mtx(4).Text = "" And emjogo Then
+            btn5.Text = "X"
+            cont += 1
+            checkvit()
+            trocaturno()
+        ElseIf turno = 1 And mtx(4).Text = "" And emjogo Then
+            btn5.Text = "O"
+            cont += 1
+            checkvit()
+            trocaturno()
+        End If
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        If turno = 0 And mtx(5).Text = "" And emjogo Then
+            btn6.Text = "X"
+            cont += 1
+            checkvit()
+            trocaturno()
+        ElseIf turno = 1 And mtx(5).Text = "" And emjogo Then
+            btn6.Text = "O"
+            cont += 1
+            checkvit()
+            trocaturno()
+        End If
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        If turno = 0 And mtx(6).Text = "" And emjogo Then
+            btn7.Text = "X"
+            cont += 1
+            checkvit()
+            trocaturno()
+        ElseIf turno = 1 And mtx(6).Text = "" And emjogo Then
+            btn7.Text = "O"
+            cont += 1
+            checkvit()
+            trocaturno()
+        End If
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        If turno = 0 And mtx(7).Text = "" And emjogo Then
+            btn8.Text = "X"
+            cont += 1
+            checkvit()
+            trocaturno()
+        ElseIf turno = 1 And mtx(7).Text = "" And emjogo Then
+            btn8.Text = "O"
+            cont += 1
+            checkvit()
+            trocaturno()
+        End If
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        If turno = 0 And mtx(8).Text = "" And emjogo Then
+            btn9.Text = "X"
+            cont += 1
+            checkvit()
+            trocaturno()
+        ElseIf turno = 1 And mtx(8).Text = "" And emjogo Then
+            btn9.Text = "O"
+            cont += 1
+            checkvit()
+            trocaturno()
+        End If
+    End Sub
+
+    Private Sub trocaturno()
+
+        If turno = 0 Then
+
+            If bot And cont < 9 And emjogo Then
+
+                turno = 1
+
+                Dim x As Integer = Math.Ceiling(Rnd() * 8)
+                While mtx(x).Text <> ""
+                    x = Math.Ceiling(Rnd() * 8)
+                End While
+                mtx(x).Text = "O"
+                checkvit()
+                trocaturno()
+
+            Else
+                turno = 1
+                lblTurn.Text = "Vez de: " & txtName2.Text
+            End If
+
+        ElseIf turno = 1 Then
+
+            turno = 0
+            lblTurn.Text = "Vez de: " & txtName1.Text
+
+        End If
+
+    End Sub
+
+    Private Sub checkvit()
+
+        If turno = 0 Then
+
+            If mtx(0).Text = "X" And mtx(1).Text = "X" And mtx(2).Text = "X" Then vitoria(1)
+            If mtx(3).Text = "X" And mtx(4).Text = "X" And mtx(5).Text = "X" Then vitoria(1)
+            If mtx(6).Text = "X" And mtx(7).Text = "X" And mtx(8).Text = "X" Then vitoria(1)
+
+            If mtx(0).Text = "X" And mtx(3).Text = "X" And mtx(6).Text = "X" Then vitoria(1)
+            If mtx(1).Text = "X" And mtx(4).Text = "X" And mtx(7).Text = "X" Then vitoria(1)
+            If mtx(2).Text = "X" And mtx(5).Text = "X" And mtx(8).Text = "X" Then vitoria(1)
+
+            If mtx(0).Text = "X" And mtx(4).Text = "X" And mtx(8).Text = "X" Then vitoria(1)
+            If mtx(2).Text = "X" And mtx(4).Text = "X" And mtx(6).Text = "X" Then vitoria(1)
+
+        ElseIf turno = 1 Then
+
+            If mtx(0).Text = "O" And mtx(1).Text = "O" And mtx(2).Text = "O" Then vitoria(2)
+            If mtx(3).Text = "O" And mtx(4).Text = "O" And mtx(5).Text = "O" Then vitoria(2)
+            If mtx(6).Text = "O" And mtx(7).Text = "O" And mtx(8).Text = "O" Then vitoria(2)
+
+            If mtx(0).Text = "O" And mtx(3).Text = "O" And mtx(6).Text = "O" Then vitoria(2)
+            If mtx(1).Text = "O" And mtx(4).Text = "O" And mtx(7).Text = "O" Then vitoria(2)
+            If mtx(2).Text = "O" And mtx(5).Text = "O" And mtx(8).Text = "O" Then vitoria(2)
+
+            If mtx(0).Text = "O" And mtx(4).Text = "O" And mtx(8).Text = "O" Then vitoria(2)
+            If mtx(2).Text = "O" And mtx(4).Text = "O" And mtx(6).Text = "O" Then vitoria(2)
+
+        End If
+
+        If cont = 9 And emjogo Then vitoria(3)
+
+    End Sub
+
+    Private Sub vitoria(vit As Integer)
+
+        play1 = txtName1.Text
+        play2 = txtName2.Text
+
+        If vit = 1 Then
+
+            p1point += 1
+            lblPoint1.Text = CStr(p1point)
+            lblVitoria.Text = "VITÓRIA DE " & UCase(txtName1.Text)
+
+        ElseIf vit = 2 Then
+
+            p2point += 1
+            lblPoint2.Text = CStr(p2point)
+            lblVitoria.Text = "VITÓRIA DE " & UCase(txtName2.Text)
+
+        ElseIf vit = 3 Then
+
+            lblVitoria.Text = "!!! EMPATE !!!"
+
+        End If
+
+        lblVitoria.Visible = True
+        btnReplay.Visible = True
+        emjogo = False
+
+    End Sub
+
+    Private Sub btnReplay_Click(sender As Object, e As EventArgs) Handles btnReplay.Click
+
+        Me.Controls.Clear()
+        InitializeComponent()
+        Form1_Load(e, e)
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+        If emjogo And cont > 0 Then
+            play1 = txtName1.Text
+            play2 = txtName2.Text
+
+            Me.Controls.Clear()
+            InitializeComponent()
+            Form1_Load(e, e)
+        End If
+
+    End Sub
+
+    Private Sub chbBot_CheckedChanged(sender As Object, e As EventArgs) Handles chbBot.CheckedChanged
+
+        If bot Then
+            bot = False
+        Else
+            bot = True
+        End If
+
+    End Sub
+End Class
